37da5ed34211506a4e89e0153e1231275f3d8422
clementine-smart <cmwsmart@gmail.com>
2023-02-02 15:23:16 +1300

    Add footer

---
+++ app/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="flex justify-end w-full p-8 gap-9">
+      <p className="text-5xl self-center font-light">SOFTWARE DEVELOPER</p>
+      <p className="text-5xl font-bold">SMART</p>
+    </footer>
+  )
+}
+
+export default Footer
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Header from './Header'
 import Nav from './Nav'
+import Footer from './Footer'
 
 export default function RootLayout({
   children,
@@ -11,12 +12,13 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <div className="flex">
-          <header className="flex justify-between w-screen m-8">
+        <div className="flex flex-col justify-between h-screen">
+          <header className="flex justify-between w-full p-8">
             <Header />
             <Nav />
           </header>
           {children}
+          <Footer />
         </div>
       </body>
     </html>
