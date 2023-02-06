0789250196973665b645caa3a17493d231732a6e
clementine-smart <cmwsmart@gmail.com>
2023-02-07 00:24:53 +1300

    Update headers to links

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
-    <footer className="flex justify-end w-full p-8 gap-9">
+    <footer className="flex justify-end w-full p-8 gap-9 select-none">
       <p className="text-2xl self-center font-light tracking-widest">
-        SOFTWARE DEVELOPER
+        <Link href="/">SOFTWARE DEVELOPER</Link>
       </p>
-      <p className="text-5xl font-bold tracking-wide">SMART</p>
+
+      <p className="text-5xl font-bold tracking-wide">
+        <Link href="/">SMART </Link>
+      </p>
     </footer>
   )
 }
--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,9 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <h1 className="text-5xl font-bold tracking-wide select-none">CLEMENTINE</h1>
+    <h1 className="text-5xl font-bold tracking-wide select-none">
+      <Link href="/">CLEMENTINE</Link>
+    </h1>
   )
 }
